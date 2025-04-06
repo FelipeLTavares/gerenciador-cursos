07982f78a6a64ab99ebcebe72307fa6e7d7918bd
Felipe Tavares <felipelimatavares96@gmail.com>
2025-04-06 17:36:49 -0300

    front: Voltar para tela e login caso nao esteja com um token valido

--- front/src/app/auth/auth-errors.interceptor.ts
+++ front/src/app/auth/auth-errors.interceptor.ts
@@ -1,16 +1,19 @@
-import { HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { Router } from '@angular/router';
+import { HttpInterceptorFn } from '@angular/common/http';
 import { catchError, throwError } from 'rxjs';
+import { NgZone } from '@angular/core';
 
 export const authErrorInterceptor: HttpInterceptorFn = (req, next) => {
   const router = inject(Router);
+  const ngZone = inject(NgZone);
 
   return next(req).pipe(
     catchError((error) => {
       if (error.status === 401) {
-        // localStorage.removeItem('token');
-        router.navigate(['/auth/login']);
+        ngZone.run(() => {
+          router.navigate(['/login']);
+        });
       }
 
       return throwError(() => error);
